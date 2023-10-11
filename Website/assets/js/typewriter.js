6121c8e8eb09cc42e9e80069e8f94ec2559cd7ab
Waseq225 <mohammadwaseq225@gmail.com>
2023-10-11 17:23:06 +1100

    added typewriter effect to index.html

---
+++ Website/assets/js/typewriter.js
@@ -0,0 +1,35 @@
+const sentences = [
+    " My name is Waseq",
+    " I am a Solutions Architect",
+    " I am a Machine Learning enthusiast",
+    " I play video games",
+    " I love my cat"
+];
+
+const typewriter = document.getElementById('typewriter');
+let sentenceIndex = 0;
+let charIndex = 0;
+
+function typeSentence() {
+    if (charIndex < sentences[sentenceIndex].length) {
+        typewriter.innerHTML += sentences[sentenceIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeSentence, 50); // Adjust typing speed here
+    } else {
+        setTimeout(eraseSentence, 2000); // Adjust pause before erasing here
+    }
+}
+
+function eraseSentence() {
+    if (charIndex > 1) {
+        typewriter.innerHTML = sentences[sentenceIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(eraseSentence, 50); // Adjust erasing speed here
+    } else {
+        sentenceIndex = (sentenceIndex + 1) % sentences.length;
+        setTimeout(typeSentence, 300); // Adjust pause between sentences here
+    }
+}
+
+// Start the typewriter effect
+typeSentence();
